3016c8311538451650e181b4f2fd43c6cc272230
aliciabayerl <abayerl@uni-osnabrueck.de>
2024-06-09 20:44:06 +0200

    Fehlerbehebung mit alter Version von mir (PSQ20)

--- POMS/transform_scores.py
+++ POMS/transform_scores.py
@@ -32,6 +32,21 @@ def convert_deq_values(value):
         print("Unexpected value:", value)
         return value
 
+def convert_psq_values(value):
+    if value == "Almost never ":
+        return 0
+    elif value == "Rarely":
+        return 1
+    elif value == "Sometimes":
+        return 2
+    elif value == "Often":
+        return 3
+    elif value == "Always":
+        return 4
+    else:
+        return int(value)
+
+    
 
 # Function to convert poms values
 def convert_poms_columns(df):
@@ -46,6 +61,14 @@ def convert_deq_columns(df):
         if "DEQ Before >>" in column or "DEQ After >>" in column:
             df[column] = df[column].apply(convert_deq_values)
     return df
+
+# Function to convert psq values
+def convert_psq_columns(df):
+    for column in df.columns:
+        if "PSQ20 >>" in column:
+            df[column] = df[column].apply(convert_psq_values)
+    return df
+
 
 
 # Function to calculate score with mapping POMS
@@ -105,7 +128,15 @@ def calculate_deq_scores(df):
                                                     'Before Desire', 'After Desire', 
                                                     'Before Relaxation', 'After Relaxation',
                                                     'Before Happiness', 'After Happiness'])
+
+    print("Columns before assignment:")
+    print(participant_scores_deq.columns)
     
+    # Loop through participants and calculate scores
+    for index, participant in df.iterrows():
+        # Score calculation logic
+        print("Columns after assignment:")
+        print(participant_scores_deq.columns)
 
     for index, participant in df.iterrows():
         participant_scores_deq.loc[index] = [index + 1, participant['Condition']] + [0] * (len(participant_scores_deq.columns) - 2)
@@ -125,7 +156,42 @@ def calculate_deq_scores(df):
     
     return participant_scores_deq
 
+# Function to calculate score with mapping psq
+def calculate_psq_scores(df):
+    score_mappings = {
+        'Worries': ['You fear you may not manage to attain your goals', 'You feel frustrated', 'Your problems seem to be piling up', 'You have many worries', 'You are afraid for the future'],
+        'Tension': ['You feel rested', 'You feel calm', 'You feel tense', 'You feel mentally exhausted', 'You have trouble relaxing'],
+        'Joy': ['You feel you’re doing things you really like', 'You are full of energy', 'You feel safe and protected', 'You enjoy yourself', 'You are lighthearted'],
+        'Demands': ['You feel that too many demands are being made on you', 'You have too many things to do', 'You feel you’re in a hurry', 'You have enough time for yourself', 'You feel under pressure from deadlines']
+    }
+    
+    # Seperate data to store before and after scores 
+    participant_scores_psq = pd.DataFrame(columns=['Participant', 'Condition', 
+                                                    'Worries', 'Tension', 
+                                                    'Joy', 'Demands'])
 
+    print("Columns before assignment:")
+    print(participant_scores_psq.columns)
+    
+    # Loop through participants and calculate scores
+    for index, participant in df.iterrows():
+        # Score calculation logic
+        print("Columns after assignment:")
+        print(participant_scores_psq.columns)
+
+    for index, participant in df.iterrows():
+        participant_scores_psq.loc[index] = [index + 1, participant['Condition']] + [0] * (len(participant_scores_psq.columns) - 2)
+            
+        for score, mood_states in score_mappings.items():
+            mood_state_score = 0
+
+            for col in df.columns:
+                if "PSQ20 >>" in col and col.split(" >> ")[1] in mood_states:
+                    mood_state_score+= convert_psq_values(participant[col])
+            participant_scores_psq.at[index, f' {score}'] = mood_state_score
+
+    
+    return participant_scores_psq
 
 folder_path = 'POMS'
 input_file = 'questionnaire.csv'
@@ -135,14 +201,18 @@ data = pd.read_csv(file_path)
 
 converted_data = convert_poms_columns(data)
 converted_data2 = convert_deq_columns(data)
+converted_data3 = convert_psq_columns(data)
 
 participant_scores = calculate_poms_scores(converted_data)
 participant_scores_deq = calculate_deq_scores(converted_data2)
+participant_scores_psq = calculate_psq_scores(converted_data3)
 
 output_file = 'POMS/participant_scores.csv'
 output_file2 = 'POMS/participant_scores_deq.csv'
+output_file3 = 'POMS/participant_scores_psq.csv'
 participant_scores.to_csv(output_file, index=False)
 participant_scores_deq.to_csv(output_file2, index=False)
+participant_scores_psq.to_csv(output_file3, index=False)
 
 print("Participant scores saved to:", output_file)
 
